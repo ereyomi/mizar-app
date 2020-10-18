21425f2ef01e43f1c1c0df5493f22cfc35b83d2b
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-10-18 17:11:30 +0100

    feat: footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
@@ -8,7 +9,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   imports: [
     CommonModule,
   ],
-  declarations: [ NavBarComponent ],
-  exports: [ NavBarComponent ],
+  declarations: [ NavBarComponent, FooterComponent ],
+  exports: [ NavBarComponent, FooterComponent ],
 } )
 export class SharedModule { }
