af97eada4fb15230edbdbf91bacee244821e1a7e
Ereyomi Oluwaseyi Samuel <ereyomioluwaseyi@gmail.com>
2020-10-18 17:56:20 +0100

    feat: add animate on scroll to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mizar-app';
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
